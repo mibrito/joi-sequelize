fc71ef31a1289bf191a4a3387004999014e8e28a
Michele Brito Pinheiro <mibritom@gmail.com>
2016-04-12 22:46:16 -0300

    add support to direct type declarations on models. add tests for integer

--- test/datatypes/integer.js
+++ test/datatypes/integer.js
@@ -10,16 +10,43 @@ var JS = new JoiSequelize(model);
 
 
 describe('INTEGER', () => {
+  describe('Very Simple use without object', () => {
+    it('Should create joi schema', () => {
+      JS.joi.should.have.deep.property('very_simple');
+      JS.joi.very_simple.isJoi.should.be.ok;
+      JS.joi.very_simple._type.should.be.equal('number');
+    });
+    it('Should throw error with wrong type', () => {
+      JS.joi.very_simple.validate('aaaaa', (err)=> {
+        expect(err).to.not.be.null;
+      });
+    });
+    it('Should validate as integer', () => {
+      JS.joi.very_simple.validate(1, (err)=> {
+        expect(err).to.be.null;
+      });
+    });
+    it('Should validate allow null', () => {
+      JS.joi.very_simple.validate(null, (err) => {
+        expect(err).to.be.null;
+      });
+    });
+    it('Should doesn`t have description', function () {
+      expect(JS.joi.very_simple._description).to.be.null;
+    });
+  });
   describe('Simple use: INTEGER', () => {
     it('Should create joi schema', () => {
       JS.joi.should.have.deep.property('simple');
       JS.joi.simple.isJoi.should.be.ok;
-    });
-    it('Should validate as integer', () => {
       JS.joi.simple._type.should.be.equal('number');
+    });
+    it('Should throw error with wrong type', () => {
       JS.joi.simple.validate('aaaaa', (err)=> {
         expect(err).to.not.be.null;
       });
+    });
+    it('Should validate as integer', () => {
       JS.joi.simple.validate(1, (err)=> {
         expect(err).to.be.null;
       });
--- test/models/integer.js
+++ test/models/integer.js
@@ -2,6 +2,7 @@
 
 module.exports = (sequelize, DataTypes) => {
   var Integer = sequelize.define('Integer', {
+    very_simple: DataTypes.INTEGER,
     simple: {
       type: DataTypes.INTEGER,
       allowNull: true,
